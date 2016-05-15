f61247c77b25c202390345ab268f345a6ca8a586
Sofia Qvarfort <sofia.qvarfort.15@ucl.ac.uk>
2016-05-15 21:56:02 +0100

    Added comments

--- RK4.py
+++ RK4.py
@@ -1,8 +1,14 @@
+""" This is an RK4 solver that evolves a density matrix under some Hamiltonian, to be replaced by the full Lindblad equation.
+Changes:
+- Change density matrix to look at Bloch vector? Better intuition... But bad for future two-qubit states, can't have 4D Bloch vector...
+-
+"""
+
 import numpy as np
 from numpy import exp
 
 
-def RK4(Lindblad, rho, h, time):
+def RK4(Lindblad, rho, h, time): # RK4 solver
     k1 = Lindblad(time, rho)
     k2 = Lindblad(time + h/2., rho + h/2.*k1)
     k3 = Lindblad(time + h/2., rho + h/2.*k2)
@@ -11,38 +17,35 @@ def RK4(Lindblad, rho, h, time):
 
 
 
-sigmax = np.matrix([[0,1], [1,0]]) # This is our
-
-rho = np.matrix([[1,1],[1,1]])*1/2.
+sigmax = np.matrix([[0,1], [1,0]])
+sigmaz = np.matrix([[1,0],[0,-1]])
+rho = np.matrix([[1,1],[1,1]])*1/2. # Start in |+> state
 
 
 def commutator(A,B):
     return np.dot(A,B) - np.dot(B,A)
 
-def Heisenberg(time, rho):
-    sigmaz = np.matrix([[1,0],[0,-1]])
+def Heisenberg(time, rho): # Very simple Hamiltonian
     return -1j*commutator(sigmaz,rho)
 
 
 
-#def rho(t):
-#    return np.matrix([[1,exp(-1j*t)],[exp(1j*t),1]])*1/2
-
-h = 0.1
-time = 0.0
-
 
 import matplotlib.pyplot as plt
 plt.plot()
-plt.ylabel('some numbers')
+
+#
+h = 0.1 # Time-step
+time = 0.0 # Starting time
 
 
+# Iterate over RK4-method
 for t in range(0,20):
-    dy = RK4(Heisenberg, rho, h, time)
+    dy = RK4(Heisenberg, rho, h, time) # Invke solver
     rho = rho + dy
     time = time + h
     print rho
-    plt.scatter(time, rho[0,1])
+    plt.scatter(time, rho[0,1]) # Plot off-diagonal matrix element
     plt.scatter(time, rho[1,0], color = 'red')
 
 
