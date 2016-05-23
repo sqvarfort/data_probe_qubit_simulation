e3b4570b707e5ef07e8917b59b4896e24b00951a
Sofia Qvarfort <sofia.qvarfort.15@ucl.ac.uk>
2016-05-23 15:50:13 +0100

    Program runs and calculates correct phi values from arctan

--- Plotter.py
+++ Plotter.py
@@ -13,13 +13,13 @@ class Plotter(object):
     """ Class that calculates statistics and plots phase on histograms"""
 
     def __init__(self, states, info):
-        self.states= states
+        self.states = states
         self.info = info
         # Extract list of probe states
         probe_states = [states[t].ptrace(0) for t in range(0, len(states))]
         list_of_phi = [self.extract_phi(self.bloch_vector(probe_states[t])) for t in range(0,len(probe_states))]
         self.Histogram(list_of_phi)
-        self.save_to_file(list_of_phi)
+        self.save_to_file(list_of_phi, info)
 
     def bloch_vector(self, state): # Calculate the bloch vector
         return [expect(sigmax(), state),
@@ -27,36 +27,45 @@ class Plotter(object):
                    expect(sigmaz(), state)]
 
     def extract_phi(self, vec):
-        if vec[0] < 0:
-            return arctan(vec[1]/vec[0])
+        if vec[0] > 0.:
+            if vec[1] < 0.:
+                return -arctan(vec[1]/vec[0])
+            else:
+                return 2.*pi - arctan(vec[1]/vec[0])
         else:
-            return arctan(vec[1]/vec[0]) + pi
-
+            return pi - arctan(vec[1]/vec[0])
 
     def Histogram(self, list_of_phi):
-
-
+        path = os.path.abspath(__file__) # Set path where rates are saved to current directory
+        fig = plt.figure()
+        ax = fig.add_subplot(111,title='Phase Histogram')
+        ax.set_xlabel('phi')
+        ax.set_ylabel('N')
+        #plt.plot(list_of_phi)
         plt.hist(list_of_phi)
         plt.show()
-        ax = plt.axes()
-        plt.savefig('Histogram.pdf')
+        plt.savefig('Histogram.pdf', path)
 
     def save_to_file(self, list_of_phi, info):
-        os.chdir(path) # Set path where rates are saved to current directory
         f = open('phi_raw_data.txt', 'w')
         res.write('Values of phi \n')
         res.write('Lindblad operators:' + info)
         res.write('Number of runs:' + len(list_of_phi))
         # Write all values to the file
-        for i in len(list_of_phi):
+        for i in range(0, len(list_of_phi)):
             res.write(list_of_phi[i] + "\n")
+
         # Calculate mean and std and write them to file
-        mean_phi = mean(list_of_phi)
-        std_phi = std(list_of_phi)
+        mean_phi = self.calc_mean(list_of_phi)
+        std_phi = self.calc_std(list_of_phi)
         res.write('The mean phase is:' + mean_phi + 'which is' + abs(mean_phi - pi/2.)/(pi/2.) + "% of pi/2" + "\n")
         res.write("\n" + 'The standard deviation is:' + std_phi + "which is" + abs(std_phi - pi/2.)/(pi/2.) + "% of pi/2" + "\n")
 
+    def calc_mean(self, list_of_phi):
+        return mean(list_of_phi)
 
+    def calc_std(self, list_of_phi):
+        return std(list_of_phi)
 
 
 
