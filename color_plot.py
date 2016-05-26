7de24731da9836f2212e70830e13c2629162ca21
Simon Schaal <nomis05@gmail.com>
2016-05-26 12:47:52 +0100

    Add examples of parity measuremtn to doc

--- color_plot.py
+++ color_plot.py
@@ -5,6 +5,19 @@ from DataQubitDisplacement import *
 from Simulation import Simulation
 from qutip import *
 import yaml
+import numpy as np
+
+config = "ham_conf.yml"
+config_args = {}
+
+if type(config) == str:
+    with open(config) as cfile:
+        config_args.update(yaml.load(cfile))
+elif type(config) == dict:
+    config_args.update(config)
+else:
+    print "config is wrong type"
+
 
 """ Prepare Bloch Sphere settings """
 db=Bloch()
@@ -13,7 +26,7 @@ db.point_color = colors
 db.point_marker = ['o']
 
 
-result_states = qload("anim/notodd")
+result_states = qload("anim/odd")
 
 for i in range(4):
 	db.add_points(np.transpose(np.array([Plotter.bloch_vector(result_states[t].ptrace(0)) for t in range(int(i*config_args['steps']/4.),int((i+1)*config_args['steps']/4.),10)])))
