7d7eebdc37560c8f9d9edeaa3834b130e2e22560
simonschaal <simonschaal@users.noreply.github.com>
2016-05-26 11:06:01 +0100

    Add qsave/qload

--- color_plot_example.py
+++ color_plot_example.py
@@ -4,8 +4,8 @@ colors = ["b","g","r","k"]#,"r","g","#CC6600"]
 db.point_color = colors
 db.point_marker = ['o']
 
-
-result_states = sim.last_run_all
+# qsave(result_states, "anim/odd")
+result_states = qload("anim/odd")
 
 for i in range(4):
 	db.add_points(np.transpose(np.array([Plotter.bloch_vector(result_states[t].ptrace(0)) for t in range(int(i*config_args['steps']/4.),int((i+1)*config_args['steps']/4.),10)])))
