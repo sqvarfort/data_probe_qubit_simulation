5b33663fead52c30e591ca97865022d88cdbac3e
Simon Schaal <nomis05@gmail.com>
2016-05-26 12:48:47 +0100

    Added updated figures

--- color_plot.py
+++ color_plot.py
@@ -26,9 +26,11 @@ db.point_color = colors
 db.point_marker = ['o']
 
 
-result_states = qload("anim/odd")
+result_states = qload("anim/notodd")
 
 for i in range(4):
 	db.add_points(np.transpose(np.array([Plotter.bloch_vector(result_states[t].ptrace(0)) for t in range(int(i*config_args['steps']/4.),int((i+1)*config_args['steps']/4.),10)])))
+
+db.add_states(result_states[-1].ptrace(0))
 
 db.show()
