d24aefe5ba9bb845f4b7ac1fccfdcc86b875a112
simonschaal <nomis05@gmail.com>
2016-05-26 12:37:51 +0100

    Complete color plot

--- color_plot.py
+++ color_plot.py
@@ -1,3 +1,11 @@
+from H_RWA import H_RWA
+from Plotter import Plotter
+from random import random
+from DataQubitDisplacement import *
+from Simulation import Simulation
+from qutip import *
+import yaml
+
 """ Prepare Bloch Sphere settings """
 db=Bloch()
 colors = ["b","g","r","k"]#,"r","g","#CC6600"]
@@ -5,7 +13,7 @@ db.point_color = colors
 db.point_marker = ['o']
 
 
-result_states = sim.last_run_all
+result_states = qload("anim/notodd")
 
 for i in range(4):
 	db.add_points(np.transpose(np.array([Plotter.bloch_vector(result_states[t].ptrace(0)) for t in range(int(i*config_args['steps']/4.),int((i+1)*config_args['steps']/4.),10)])))
